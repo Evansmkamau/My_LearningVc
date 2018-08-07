afb0dd1f208e11f1091970e68e73deeb05ccc230
evans kamau <evans.mwangi.kamau@volvocars.com>
2018-08-07 11:14:56 +0200

    Edited for security p

--- Py_scriptdryrun.py
+++ Py_scriptdryrun.py
@@ -9,19 +9,19 @@ commitid = (raw_input ('Enter CommitID: '))
 
 if DryRunType == "recheck":
     print ("Recheck on Commit: ", commitid)
-    ssh -p 29421 cdsid@gotsvl1415.got.volvocars.net gerrit review -m '"!recheck"' commitid #This line has not been interpreted from shell to py
+    ssh -p 29421 cdsid@ServerAddressOrName gerrit review -m '"!recheck"' commitid #This line has not been interpreted from shell to py. Note ths Sv is hidden
         result = $?
             if result != 0:
                 print ("Commit check not executed")
 elif DryRunType == "testgate":
     print ("TestGate on Commit: ", commitid)
-    ssh -p 29421 cdsid@gotsvl1415.got.volvocars.net gerrit review -m '"!recheck"' commitid #This line has not been interpreted from shell to py
+    ssh -p 29421 cdsid@ServerAddressOrName gerrit review -m '"!recheck"' commitid #This line has not been interpreted from shell to py. Note ths Sv is hidden
         resultgate = $?
             if result != 0:
                 print ("Gate test not executed")
 elif DryRunType == "testhourly":
     print ("TestHourly on Commit: ", commitid)
-    ssh -p 29421 cdsid@gotsvl1415.got.volvocars.net gerrit review -m '"!recheck"' commitid #This line has not been interpreted from shell to py
+    ssh -p 29421 cdsid@ServerAddressOrName gerrit review -m '"!recheck"' commitid #This line has not been interpreted from shell to py. Note ths Sv is hidden
         resulthourly = $?
             if result != 0:
                 print ("Hourly dry run not executed")
